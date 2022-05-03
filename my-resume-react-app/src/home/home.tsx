502486e70cd5a86fd4d5f9db8d28d8e0a50f0b74
Simon <simon.vereecke@live.com>
2022-05-03 21:57:57 +0200

    Add intro card

--- my-resume-react-app/src/home/home.tsx
+++ my-resume-react-app/src/home/home.tsx
@@ -4,6 +4,9 @@ import {
   AppBar,
   Box,
   Button,
+  Card,
+  CardActions,
+  CardContent,
   Toolbar,
   Typography,
 } from "@mui/material";
@@ -11,19 +14,47 @@ import { DeveloperMode } from "@mui/icons-material";
 
 interface Props {}
 
-export const Home: React.FC<Props> = (props) => {
+export const Home: React.FC = (props) => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-         
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
             <DeveloperMode className="padding-right" />
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            Simon Vereecke
-          </Typography>
-          <Button color="inherit">Contact</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Simon Vereecke
+            </Typography>
+            <Button color="inherit">Contact</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div className="card">
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Hi
+            </Typography>
+            <Typography variant="h5" component="div">
+              I'm Simon
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Full-stack developer
+            </Typography>
+            <Typography variant="body2">
+              My main areas of expertise
+              <br />
+              include C#, Angular and React.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">See my work experience</Button>
+          </CardActions>
+        </Card>
+      </div>
+    </>
   );
 };
