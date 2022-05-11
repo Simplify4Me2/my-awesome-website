d43fc61a670c94ce7e5a072be207f7cc27a30434
Simon <simon.vereecke@live.com>
2022-05-11 15:26:23 +0200

    Add basic career timeline

--- my-resume-react-app/src/home/career-timeline.tsx
+++ my-resume-react-app/src/home/career-timeline.tsx
@@ -1,6 +1,57 @@
 import React from "react";
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator,
+} from "@mui/lab";
 
 export const CareerTimeline: React.FC = (props) => {
-  {/* https://mui.com/material-ui/react-timeline/ */}
-    return <div>Timeline</div>
-}
+  return (
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2020 - Current
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Protime</TimelineContent>
+      </TimelineItem>
+      {/* <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          10:00 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem> */}
+      {/* <TimelineItem>
+        <TimelineOppositeContent color="text.secondary">
+          2017 - 2020
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Cegeka</TimelineContent>
+      </TimelineItem> */}
+      <TimelineItem>
+        <TimelineOppositeContent >
+          Cegeka
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent color="text.secondary">2017 - 2020</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+};
