4eb095f0370db583be1396cab3362e0d5145537c
Simon <simon.vereecke@live.com>
2022-05-29 22:17:19 +0200

    add certifficate badges

--- my-resume-react-app/src/home/home.tsx
+++ my-resume-react-app/src/home/home.tsx
@@ -9,6 +9,11 @@ export const Home: React.FC = (props) => {
       <Header />
       <div className="flex flex-col items-center">
         <IntroductionCard />
+        <div className="flex m-10">
+          <img className="h-60 m-4" src="https://images.credly.com/images/be8fcaeb-c769-4858-b567-ffaaa73ce8cf/image.png" />
+          <img className="h-60 m-4" src="https://images.credly.com/images/63316b60-f62d-4e51-aacc-c23cb850089c/azure-developer-associate-600x600.png" />
+          <img className="h-60 m-4" src="https://images.credly.com/images/c3ab66f8-5d59-4afa-a6c2-0ba30a1989ca/CERT-Expert-DevOps-Engineer-600x600.png" />
+        </div>
         <CareerTimeline />
       </div>
     </>
--- my-resume-react-app/src/home/introduction-card.tsx
+++ my-resume-react-app/src/home/introduction-card.tsx
@@ -10,29 +10,35 @@ import {
 export const IntroductionCard: React.FC = (props) => {
   return (
     <Card className="justify-content m-3 pt-5" sx={{ minWidth: 275 }}>
-      <img className="h-72 m-5" src="/index-hero.jpg" alt="azure-themed-hero" />
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Hi
-        </Typography>
-        <Typography variant="h5" component="div">
-          I'm Simon
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Full-stack developer
-        </Typography>
-        <Typography variant="body2">
-          My main areas of expertise
-          <br />
-          include C#, Angular and React.
-        </Typography>
-        <Typography variant="body2">
-          I like to craft solid and scalable frontend products with
-          <br />
-          great user experience.
-        </Typography>
-      </CardContent>
-      <CardActions>
+      <div className="flex">
+        <CardContent className="mt-12">
+          <Typography  sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Hi
+          </Typography>
+          <Typography variant="h5" component="div">
+            I'm Simon
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Full-stack developer
+          </Typography>
+          <Typography variant="body2">
+            My main areas of expertise
+            <br />
+            include C#, Angular and React.
+          </Typography>
+          <Typography variant="body2">
+            I like to craft solid and scalable frontend products with
+            <br />
+            great user experience.
+          </Typography>
+        </CardContent>
+        <img
+          className="h-72 m-5"
+          src="/index-hero.jpg"
+          alt="azure-themed-hero"
+        />
+      </div>
+      <CardActions className="flex justify-center">
         <Button size="small">See my work experience</Button>
       </CardActions>
     </Card>
