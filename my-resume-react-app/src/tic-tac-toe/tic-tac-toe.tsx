337f5a61a6fcd557f440b496739cce3e4e9aadd0
Simon <simon.vereecke@live.com>
2022-06-02 21:55:16 +0200

    use a grid for tic-tac-toe

--- my-resume-react-app/src/tic-tac-toe/tic-tac-toe.tsx
+++ my-resume-react-app/src/tic-tac-toe/tic-tac-toe.tsx
@@ -2,6 +2,19 @@ import React from "react";
 
 export const TicTacToe: React.FC = () => {
   return (
+      <>
     <span>Tic Tac ToDo</span>
+    <div className="grid grid-cols-3">
+        <div className="border-2">x1</div>
+        <div className="border-2">x2</div>
+        <div className="border-2">x3</div>
+        <div className="border-2">x4</div>
+        <div className="border-2">x5</div>
+        <div className="border-2">x6</div>
+        <div className="border-2">x7</div>
+        <div className="border-2">x8</div>
+        <div className="border-2">x9</div>
+    </div>
+      </>
   );
 };
