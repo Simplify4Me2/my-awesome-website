209e578e90ebe76f8863eed0bb00e040ef7e4996
Simon <simon.vereecke@live.com>
2022-06-02 20:45:14 +0200

    add Portfolio section

---
+++ my-resume-react-app/src/home/portfolio.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Portfolio: React.FC = () => {
+  return (
+    <div className="m-6">
+      <h1>Portfolio</h1>
+      <span>
+        TODO: Add some small fun projects like{" "}
+        <a href="https://michaellackey.com/" target="_blank" rel="noreferrer">
+          this
+        </a>
+      </span>
+    </div>
+  );
+};
