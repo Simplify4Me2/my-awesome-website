3da21570f2df65aaeaa2cdfc3526897ba3da1c0d
Simon <simon.vereecke@live.com>
2022-06-20 22:47:05 +0200

    fix pipeline

--- my-resume-react-app/src/tic-tac-toe/useTicTacToe.tsx
+++ my-resume-react-app/src/tic-tac-toe/useTicTacToe.tsx
@@ -3,16 +3,19 @@ import { Mark } from "./mark.model";
 
 const BOARD_SIZE: number = 9;
 
-const newGame: string[] = new Array<string>(BOARD_SIZE);
+const newGame: (Mark | undefined)[] = new Array<Mark | undefined>(BOARD_SIZE);
 
 export const useTicTacToe = () => {
-    const [state, setState] = useState<string[]>(newGame);
+  const [state, setState] = useState< (Mark | undefined)[]>(newGame);
 
-    const setMark = (mark: Mark, position: number) => {
-        setState({...state, [position]: mark});
-    };
+  const setMark = (mark: Mark, position: number) => {
+    setState({ ...state, [position]: mark });
+  };
 
-    const verifyWinner = (): boolean => false;
+  const verifyWinner = (): boolean => {
+    if (state[2] === Mark.nought) return true;
+    return false;
+  };
 
-    return {spaces: BOARD_SIZE, setMark, verifyWinner};
-}
+  return { spaces: BOARD_SIZE, setMark, verifyWinner };
+};
