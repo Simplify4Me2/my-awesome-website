7c770924c0185ceab072966a2a57437360acf453
Simon <simon.vereecke@live.com>
2022-06-22 16:43:31 +0200

    expand test verifyWinner

--- my-resume-react-app/src/tic-tac-toe/useTicTacToe.test.tsx
+++ my-resume-react-app/src/tic-tac-toe/useTicTacToe.test.tsx
@@ -1,27 +1,169 @@
-import { renderHook } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { renderHook } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
-import { Mark } from './mark.model';
-import { useTicTacToe } from './useTicTacToe';
+import { Mark } from "./mark.model";
+import { useTicTacToe } from "./useTicTacToe";
 
 describe("useTicTacToe", () => {
-    it("has a winner if there are 3 horizontal noughts in a row", () => {
-        const { result } = renderHook(() => useTicTacToe());
+  describe("verifyWinner", () => {
+    it("has a winner if there are 3 identical horizontal marks on the first row", () => {
+      const { result } = renderHook(() => useTicTacToe());
 
+      const initialResult = result.current.verifyWinner();
+      expect(initialResult).toBe(false);
+
+      act(() => {
+        result.current.setMark(Mark.nought, 0);
+      });
+      act(() => {
+        result.current.setMark(Mark.nought, 1);
+      });
+      act(() => {
+        result.current.setMark(Mark.nought, 2);
+      });
+
+      const endResult = result.current.verifyWinner();
+      expect(endResult).toBe(true);
+    });
+
+    it("has a winner if there are 3 identical horizontal marks on the second row", () => {
+      const { result } = renderHook(() => useTicTacToe());
+
+      const initialResult = result.current.verifyWinner();
+      expect(initialResult).toBe(false);
+
+      act(() => {
+        result.current.setMark(Mark.cross, 3);
+      });
+      act(() => {
+        result.current.setMark(Mark.cross, 4);
+      });
+      act(() => {
+        result.current.setMark(Mark.cross, 5);
+      });
+
+      const endResult = result.current.verifyWinner();
+      expect(endResult).toBe(true);
+    });
+
+    it("has a winner if there are 3 identical horizontal marks on the third row", () => {
+      const { result } = renderHook(() => useTicTacToe());
+
+      const initialResult = result.current.verifyWinner();
+      expect(initialResult).toBe(false);
+
+      act(() => {
+        result.current.setMark(Mark.nought, 6);
+      });
+      act(() => {
+        result.current.setMark(Mark.nought, 7);
+      });
+      act(() => {
+        result.current.setMark(Mark.nought, 8);
+      });
+
+      const endResult = result.current.verifyWinner();
+      expect(endResult).toBe(true);
+    });
+
+    it("has a winner if there are 3 identical vertical marks in the first column", () => {
+        const { result } = renderHook(() => useTicTacToe());
+  
         const initialResult = result.current.verifyWinner();
         expect(initialResult).toBe(false);
-
+  
         act(() => {
-            result.current.setMark(Mark.nought, 0);
+          result.current.setMark(Mark.cross, 0);
         });
         act(() => {
-            result.current.setMark(Mark.nought, 1);
+          result.current.setMark(Mark.cross, 3);
         });
         act(() => {
-            result.current.setMark(Mark.nought, 2);
+          result.current.setMark(Mark.cross, 6);
         });
-
+  
         const endResult = result.current.verifyWinner();
         expect(endResult).toBe(true);
-    });
+      });
+
+      it("has a winner if there are 3 identical vertical marks in the second column", () => {
+        const { result } = renderHook(() => useTicTacToe());
+  
+        const initialResult = result.current.verifyWinner();
+        expect(initialResult).toBe(false);
+  
+        act(() => {
+          result.current.setMark(Mark.nought, 1);
+        });
+        act(() => {
+          result.current.setMark(Mark.nought, 4);
+        });
+        act(() => {
+          result.current.setMark(Mark.nought, 7);
+        });
+  
+        const endResult = result.current.verifyWinner();
+        expect(endResult).toBe(true);
+      });
+
+      it("has a winner if there are 3 identical vertical marks in the third column", () => {
+        const { result } = renderHook(() => useTicTacToe());
+  
+        const initialResult = result.current.verifyWinner();
+        expect(initialResult).toBe(false);
+  
+        act(() => {
+          result.current.setMark(Mark.cross, 2);
+        });
+        act(() => {
+          result.current.setMark(Mark.cross, 5);
+        });
+        act(() => {
+          result.current.setMark(Mark.cross, 8);
+        });
+  
+        const endResult = result.current.verifyWinner();
+        expect(endResult).toBe(true);
+      });
+
+      it("has a winner if there are 3 identical diagonal marks from the top left corner", () => {
+        const { result } = renderHook(() => useTicTacToe());
+  
+        const initialResult = result.current.verifyWinner();
+        expect(initialResult).toBe(false);
+  
+        act(() => {
+          result.current.setMark(Mark.nought, 0);
+        });
+        act(() => {
+          result.current.setMark(Mark.nought, 4);
+        });
+        act(() => {
+          result.current.setMark(Mark.nought, 8);
+        });
+  
+        const endResult = result.current.verifyWinner();
+        expect(endResult).toBe(true);
+      });
+
+      it("has a winner if there are 3 identical diagonal marks from the top right corner", () => {
+        const { result } = renderHook(() => useTicTacToe());
+  
+        const initialResult = result.current.verifyWinner();
+        expect(initialResult).toBe(false);
+  
+        act(() => {
+          result.current.setMark(Mark.cross, 2);
+        });
+        act(() => {
+          result.current.setMark(Mark.cross, 4);
+        });
+        act(() => {
+          result.current.setMark(Mark.cross, 6);
+        });
+  
+        const endResult = result.current.verifyWinner();
+        expect(endResult).toBe(true);
+      });
+  });
 });
--- my-resume-react-app/src/tic-tac-toe/useTicTacToe.tsx
+++ my-resume-react-app/src/tic-tac-toe/useTicTacToe.tsx
@@ -6,14 +6,43 @@ const BOARD_SIZE: number = 9;
 const newGame: (Mark | undefined)[] = new Array<Mark | undefined>(BOARD_SIZE);
 
 export const useTicTacToe = () => {
-  const [state, setState] = useState< (Mark | undefined)[]>(newGame);
+  const [state, setState] = useState<(Mark | undefined)[]>(newGame);
 
   const setMark = (mark: Mark, position: number) => {
     setState({ ...state, [position]: mark });
   };
 
   const verifyWinner = (): boolean => {
-    if (state[2] === Mark.nought) return false;
+    return (
+      verifyWinnerHorizontal() ||
+      verifyWinnerVertical() ||
+      verifyWinnerDiagonal()
+    );
+  };
+
+  const verifyWinnerHorizontal = (): boolean => {
+    if (state[0] !== undefined)
+      return state[0] === state[1] && state[1] === state[2];
+    if (state[3] !== undefined)
+      return state[3] === state[4] && state[4] === state[5];
+    if (state[6] !== undefined)
+      return state[6] === state[7] && state[7] === state[8];
+    return false;
+  };
+
+  const verifyWinnerVertical = (): boolean => {
+    if (state[0] !== undefined)
+      return state[0] === state[3] && state[3] === state[6];
+    if (state[1] !== undefined)
+      return state[1] === state[4] && state[4] === state[7];
+    if (state[2] !== undefined)
+      return state[2] === state[5] && state[5] === state[8];
+    return false;
+  };
+
+  const verifyWinnerDiagonal = (): boolean => {
+    if (state[4] !== undefined)
+      return (state[0] === state[4] && state[4] === state[8]) || (state[2] === state[4] && state[4] === state[6]);
     return false;
   };
 
