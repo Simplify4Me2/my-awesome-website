0d6473e4b42274cd6fc45280bb19af7e02ef77ba
Simon <simon.vereecke@live.com>
2022-06-27 13:15:03 +0200

    show mark larger

--- my-resume-react-app/src/tic-tac-toe/grid-space.tsx
+++ my-resume-react-app/src/tic-tac-toe/grid-space.tsx
@@ -12,7 +12,7 @@ export const GridSpace: React.FC<IGridSpaceProps> = ({position, mark, onClick}) 
   return (
     <div onClick={onClick} className="flex justify-center items-center h-56 w-56 border-2 bg-teal-500 min-w-full hover:bg-violet-600 hover:cursor-pointer">
       {/* {`${mark || ''}${position}`} */}
-      {`${mark || ''}`}
+      <h1>{`${mark || ''}`}</h1>
     </div>
   );
 };
