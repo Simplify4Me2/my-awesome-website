e8fa35c75eb4f4631469026ece958085a8ddacdc
Simon <simon.vereecke@live.com>
2022-08-31 22:13:46 +0200

    rotate the box

--- my-resume-react-app/src/experiment/experiment.tsx
+++ my-resume-react-app/src/experiment/experiment.tsx
@@ -4,22 +4,26 @@ import { Header } from "../components/header";
 import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
 
 export const Experiment: React.FC = () => {
-  // const ref = useRef<ThreeElements["mesh"]>(null);
-  // const fooRef = useRef();
 
-  // useFrame((state, delta) => console.log("something useFrame"));
+  return (
+    <>
+      <Header />
+      <span>Experiment</span>
+      <div id="canvas-container">
+        <Canvas>
+          <MyMesh />
+        </Canvas>
+      </div>
+    </>
+  );
+};
 
-  // useLayoutEffect(() => {
-  //   // const { current } = ref;
-  //   // console.log(current);
-  //   console.log("current");
-  //   // useFrame((state, delta) => {
-  //   //   ref.current.rotation.y += 0.02;
+const MyMesh: React.FC = () => {
+  const ref = useRef<ThreeElements["mesh"]>(null);
 
-  //   // });
-  // }, []);
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
 
-  // useLayoutEffect(() => {
+   // useLayoutEffect(() => {
 
   // });
 
@@ -28,20 +32,11 @@ export const Experiment: React.FC = () => {
   // });
 
   return (
-    <>
-      <Header />
-      <span>Experiment</span>
-      <div id="canvas-container">
-        <Canvas>
-          <mesh  rotation={[0.5, 0, 0]}>
-            <ambientLight intensity={0.1} />
-            <directionalLight color="red" position={[0, 0, 5]} />
-            <boxGeometry args={[2, 2, 2]} />
-            <meshStandardMaterial />
-          </mesh>
-        </Canvas>
-      </div>
-    </>
+    <mesh ref={ref} rotation={[0.5, 0, 0]}>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial />
+    </mesh>
   );
 };
-
