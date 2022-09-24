fc5e2c5b4e571e2fef4ac32590d088b4578ecc93
Simon <simon.vereecke@live.com>
2022-09-24 22:57:04 +0200

    Add Table to Toolkit component

--- my-resume-react-app/src/home/my-toolkit.tsx
+++ my-resume-react-app/src/home/my-toolkit.tsx
@@ -1,9 +1,72 @@
-import React from 'react';
-import { CareerTimeline } from './career-timeline';
+import React from "react";
+import {
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { CareerTimeline } from "./career-timeline";
 
-export const MyToolkit : React.FC = () => {
-    return <>
-    <span>My Toolkit</span>
-    <CareerTimeline />
+export const MyToolkit: React.FC = () => {
+  return (
+    <>
+      <Card className="justify-content m-3 pt-5" sx={{ minWidth: 275 }}>
+        <span>My Toolkit</span>
+        <TableContainer>
+          <Table>
+            <TableHead >
+              <TableRow>
+                <TableCell><span className="font-bold">Frontend</span></TableCell>
+                <TableCell><span className="font-bold">Backend</span></TableCell>
+                <TableCell><span className="font-bold">DevOps</span></TableCell>
+                <TableCell><span className="font-bold">UX/UI</span></TableCell>
+                <TableCell><span className="font-bold">Scrum</span></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Angular</TableCell>
+                <TableCell>C#</TableCell>
+                <TableCell>Azure DevOps</TableCell>
+                <TableCell>Figma</TableCell>
+                <TableCell>Retrospective</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>React</TableCell>
+                <TableCell>Domain-Driven Design</TableCell>
+                <TableCell>Gitlab</TableCell>
+                <TableCell>Prototyping</TableCell>
+                <TableCell>Kanban</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Redux</TableCell>
+                <TableCell>Entity Framework</TableCell>
+                <TableCell>Octopus</TableCell>
+                <TableCell>Material</TableCell>
+                <TableCell>Story Mapping</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>HTML</TableCell>
+                <TableCell>SQL Server</TableCell>
+                <TableCell>Docker</TableCell>
+                <TableCell>Invision</TableCell>
+                <TableCell>Example Mapping</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>CSS</TableCell>
+                <TableCell>CQRS</TableCell>
+                <TableCell>ARM</TableCell>
+                <TableCell>Design System</TableCell>
+                <TableCell>Agile</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Card>
+      <CareerTimeline />
     </>
-}
+  );
+};
