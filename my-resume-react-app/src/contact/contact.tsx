4ded4d5dd3a9b666d7577a73cfe5b385e980c841
Simon <simon.vereecke@live.com>
2023-05-10 22:41:23 +0200

    add email link

--- my-resume-react-app/src/contact/contact.tsx
+++ my-resume-react-app/src/contact/contact.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Header } from "../components/header";
 
 export function Contact() {
-    return <>Contact</>
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <h1>I've been expecting you.</h1>
+        <span>email: <a href="mailto:saymonfreak@gmail.com">saymonfreak@gmail.com</a></span>
+      </div>
+    </>
+  );
 }
