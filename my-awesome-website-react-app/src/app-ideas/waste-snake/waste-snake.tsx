b257b0becfc06efda64f93446aece59d2b783086
Simon <simon.vereecke@live.com>
2023-08-06 22:21:54 +0200

    add more waste snake context

--- my-awesome-website-react-app/src/app-ideas/waste-snake/waste-snake.tsx
+++ my-awesome-website-react-app/src/app-ideas/waste-snake/waste-snake.tsx
@@ -5,7 +5,20 @@ export const WasteSnake: React.FC = () => {
   return (
     <>
       <Header />
-      <span>Waste Snake</span>
+      <img src="/waste-snake.png" className="h-screen" />
+      <h2>Waste Types</h2>
+      <h3>Copy a sticky, write your item, add it to the snake</h3>
+      <ul>
+        <li>1. Transportation</li>
+        <li>2. Inventory</li>
+        <li>3. Motion</li>
+        <li>4. Waiting</li>
+        <li>5. Over-processing</li>
+        <li>6. Over-production</li>
+        <li>7. Defects</li>
+        <li>8. Wasted talent</li>
+        <li>9. Knowledge</li>
+      </ul>
     </>
   );
 };
