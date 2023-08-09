edd3504323cae770c430e42eb52efb40fd18d0e0
Simon <simon.vereecke@live.com>
2023-08-09 22:25:29 +0200

    convert binary to decimal

--- my-awesome-website-react-app/src/app-ideas/bin2dec/bin-2-dec.tsx
+++ my-awesome-website-react-app/src/app-ideas/bin2dec/bin-2-dec.tsx
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "../../components/header";
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 
 export const Bin2Dec: React.FC = () => {
-    const [state, setState] = useState<string>("");
-    const [validationState, setValidationState] = useState(false);
+    const [inputState, setInputState] = useState<string>("");
+    const [isValid, setIsValid] = useState(true);
+    const [resultState, setResultState] = useState<number | null>(null)
 
-    function onValueChange(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) {
-        setState(event.target.value);
+    const onValueChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setInputState(event.target.value);
+    }
+
+    const onConvertButtonClick = () => {
+      if (isValid) {
+        const result = parseInt(inputState, 2);
+        setResultState(result);
+      } 
     }
 
     useEffect(() => {
+      setResultState(null);
         const regex = new RegExp('^[0-1]*$');
-        setValidationState(!regex.test(state));
-    }, [state]);
+        setIsValid(regex.test(inputState));
+    }, [inputState]);
 
   return (
     <>
       <Header />
       <h1 className="mb-2 mt-0 text-5xl font-medium leading-tight text-primary text-center">
-        Bin 2 Dec
+        Binary to Decimal Converter
       </h1>
       <Box className="flex flex-col items-center" >
-        <TextField label="Enter binary digits here" onChange={onValueChange} variant="standard" className="max-w-fit mb-5" helperText={validationState && "incorrect entry"} error={validationState} inputProps={{ maxLength: 8, inputMode: 'numeric', pattern: '[0-1]*' }}  />
-        <Box component="div" sx={{ display: 'inline', marginTop: '2rem' }} >Result: </Box>
+        <TextField label="Enter binary digits here" onChange={onValueChange} variant="standard" className="max-w-fit mb-5" helperText={!isValid && "incorrect entry"} error={!isValid} inputProps={{ maxLength: 8, inputMode: 'numeric', pattern: '[0-1]*' }}  />
+        <Button onClick={onConvertButtonClick} disabled={!isValid} variant="contained" sx={{ marginTop: '2rem' }}>Convert</Button>
+        {resultState && <Box component="div" sx={{ display: 'inline', marginTop: '2rem' }} >Result: {resultState}</Box>}
       </Box>
     </>
   );
