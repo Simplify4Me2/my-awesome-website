ab9e68b60d25968e94a9207778f10184a64d1b87
Simon <simon.vereecke@live.com>
2023-08-12 21:02:03 +0200

    User can see a box with border-radius applied

--- my-awesome-website-react-app/src/app-ideas/border-radius-previewer/border-radius-previewer.tsx
+++ my-awesome-website-react-app/src/app-ideas/border-radius-previewer/border-radius-previewer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header } from "../../components/header";
+import { Box, Container } from "@mui/material";
 
 export function BorderRadiusPreviewer() {
   return (
@@ -8,6 +9,18 @@ export function BorderRadiusPreviewer() {
       <h1 className="mb-2 mt-0 text-5xl font-medium leading-tight text-primary text-center">
         Border-radius Previewer
       </h1>
+      <Container
+        sx={{ display: "flex", justifyContent: "center", margin: "2rem" }}
+      >
+        <Box
+          sx={{
+            border: "solid",
+            height: "15rem",
+            width: "15rem",
+            borderRadius: "16px",
+          }}
+        ></Box>
+      </Container>
     </>
   );
 }
