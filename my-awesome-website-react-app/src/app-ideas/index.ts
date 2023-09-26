26c00de2c535cb072eef77f71c0db560e0209b88
Simon <simon.vereecke@live.com>
2023-09-26 22:11:18 +0200

    remove index file for app-ideas

--- my-awesome-website-react-app/src/app-ideas/index.ts
+++
@@ -1,7 +0,0 @@
-export { Experiment } from "./experiment/experiment";
-export { TicTacToe } from "./tic-tac-toe/tic-tac-toe";
-export { PlanningPoker } from "./planning-poker/planning-poker";
-export { WasteSnake } from "./waste-snake/waste-snake";
-export { Bin2Dec } from "./bin2dec/bin-2-dec";
-export { BorderRadiusPreviewer } from "./border-radius-previewer/border-radius-previewer";
-export { BookFinder } from "./book-finder/book-finder";
