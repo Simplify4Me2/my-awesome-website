dfb6a2a4ea0ac8b337b1ca78eb3ff3475c08d0fd
Simon <simon.vereecke@live.com>
2024-02-11 21:33:10 +0100

    add bg color and bold font

--- my-awesome-website-next-app/src/app/qr-code/page.tsx
+++ my-awesome-website-next-app/src/app/qr-code/page.tsx
@@ -7,16 +7,23 @@ export default function QRCode() {
       <main className="bg-sky-100 h-screen">
         <Header />
 
-        <h1>QR Code Component</h1>
-        <h2>I have been expecting you</h2>
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/image-qr-code.png"
-          alt="QR code"
-          width={180}
-          height={37}
-          priority
-        />
+        <div className="flex justify-center m-10">
+          <div className="bg-white flex flex-col items-center rounded-xl">
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert rounded-xl m-2"
+              src="/image-qr-code.png"
+              alt="QR code"
+              width={180}
+              height={37}
+              priority
+            />
+            <h1 className="font-bold">Improve your front-end</h1>
+            <h1 className="font-bold">skills by building projects</h1>
+            <h2 className="text-sm">Scan the QR code to visit Frontend</h2>
+            <h2 className="text-sm">Mentor and take your coding skills to</h2>
+            <h2 className="text-sm">the next level</h2>
+          </div>
+        </div>
       </main>
     </>
   );
