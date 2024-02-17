7d3de20b3823dc8e937d22afa5f74270dd4daa93
Simon <simon.vereecke@live.com>
2024-02-17 19:47:42 +0100

    refine margin and padding

--- my-awesome-website-next-app/src/app/qr-code/page.tsx
+++ my-awesome-website-next-app/src/app/qr-code/page.tsx
@@ -8,20 +8,20 @@ export default function QRCode() {
         <Header />
 
         <div className="flex justify-center m-10">
-          <div className="font-outfit bg-white flex flex-col items-center rounded-xl">
+          <div className="font-outfit bg-white flex flex-col items-center rounded-xl p-2">
             <Image
               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert rounded-xl m-2"
               src="/image-qr-code.png"
               alt="QR code"
-              width={180}
+              width={200}
               height={37}
               priority
             />
-            <h1 className="font-bold flex flex-col items-center">
+            <h1 className="font-bold flex flex-col items-center m-2">
               <span>Improve your front-end</span>
               <span>skills by building projects</span>
             </h1>
-            <p className="font-normal text-sm text-slate-400 flex flex-col items-center">
+            <p className="font-normal text-xs text-slate-400 flex flex-col items-center mb-6">
               <span>Scan the QR code to visit Frontend</span>
               <span>Mentor and take your coding skills to</span>
               <span>the next level</span>
